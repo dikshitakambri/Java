fc0a4c9110f4873c22fd76236c73333ef2287738
dikshitakambri <dikshitakambri18@gmail.com>
2022-06-27 19:31:05 +0530

    Input

---
+++ Input.java
@@ -0,0 +1,9 @@
+public class Input {
+	public static void main(String[] args){
+		System.out.print("Hello ");
+		System.out.print("world");
+		System.out.println(" ");
+		System.out.println("Hello");
+		System.out.println("world");
+	}
+}
